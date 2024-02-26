3f71b5173917eb0a920c267ec2e2af1c37da9823
Giulio Foletto <giulio.foletto@outlook.com>
2024-02-26 12:25:28 +0100

    Analysis script replaced by mode of operation in main

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ if __name__ == '__main__':
     import argparse
     import json
     from manager import Manager, default_configuration
+    from analyzer import Analyzer
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--configuration", type = str, default = "", help = "Configuration file")
+    parser.add_argument("mode", type = str, help = "Mode of operation [run|analyze]")
+    parser.add_argument("file", type = str, nargs = "?", default = "", help = "Configuration file for run or data file for analyze")
     args = parser.parse_args()
 
-    try:
-        with open(args.configuration) as f:
-            configuration = json.load(f)
-    except:
-        configuration = default_configuration
+    if args.mode == "run":
+        try:
+            with open(args.file) as f:
+                configuration = json.load(f)
+        except:
+            configuration = default_configuration
 
-    m = Manager(configuration)
+        m = Manager(configuration)
+    elif args.mode == "analyze":
+        a = Analyzer(args.file)
 
