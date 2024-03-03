c78ce504f57d741b3d7731211eca1d3632267c19
Giulio Foletto <giulio.foletto@outlook.com>
2024-03-03 18:19:21 +0100

    Add analysis of load and temperature in time

--- rethebes/analysis/analysis.py
+++ rethebes/analysis/analysis.py
@@ -87,6 +87,35 @@ def plot_voltage(data, axis):
     format_standard_axis(data, axis)
 
 def plot_temp_vs_load(data, axis):
+    delta_t = data["Time"].diff().mean()/np.timedelta64(1, "s")
+    window = int(10 / delta_t)
+    axis.plot(data["Load CPU Total"].rolling(window).mean(), data["Temperature Core Average"].rolling(window).mean(), color = "g", marker = ".", linestyle = "none", label = "Avg")
+    axis.plot(data["Load CPU Total"].rolling(window).mean(), data["Temperature Core Max"].rolling(window).mean(), color = "r", marker = ".", linestyle = "none", label = "Max")
+    axis.set_xlabel("Load CPU Total [%]")
+    axis.set_ylabel("Temperature [°C]")
+    axis.grid()
+    axis.legend()
+    axis.autoscale(tight=True)
+
+def plot_time_load_temp(data, axis):
+    delta_t = data["Time"].diff().mean()/np.timedelta64(1, "s")
+    window = int(10 / delta_t)
+    axis.plot(data["Time"], data["Load CPU Total"].rolling(window).mean(), color = "0.5", marker = ".", linestyle = "none", label = "Load")
+    axistwin = axis.twinx()
+    axistwin.plot(data["Time"], data["Temperature Core Average"].rolling(window).mean(), color = "g", marker = ".", linestyle = "none", label = "Temp. Avg")
+    axistwin.plot(data["Time"], data["Temperature Core Max"].rolling(window).mean(), color = "r", marker = ".", linestyle = "none", label = "Temp. Max")
+    axis.autoscale(tight = True)
+    axis.grid()
+    axis.legend()
+    axis.xaxis.set_major_formatter(date_format)
+    axis.xaxis.set_major_locator(mdates.SecondLocator(interval=get_best_tick_interval(data)))
+    axis.set_xlabel("Time")
+    axis.set_ylabel("Load CPU Total [%]")
+    axistwin.legend()
+    axistwin.set_ylabel("Temperature [°C]")
+    axistwin.autoscale(tight = True)
+
+def plot_temp_vs_load_per_core_sorted(data, axis):
     window = 20
     num_cores = get_number_of_cores(data)
     for i in range(num_cores):
@@ -130,9 +159,21 @@ def all_plots(data):
     fig.autofmt_xdate()
     fig.tight_layout()
     
-    fig2, ax_temp_vs_load = plt.subplots()
-    plot_temp_vs_load(data, ax_temp_vs_load)
+    fig2 = plt.figure()
+    ax_time_load_temp = fig2.add_subplot(121)
+    plot_time_load_temp(data, ax_time_load_temp)
+
+    ax_temp_vs_load = fig2.add_subplot(122)
+    plot_temp_vs_load(data, ax_temp_vs_load)    
+
+    fig2.autofmt_xdate()
     fig2.tight_layout()
+
+    fig3 = plt.figure()
+    ax_temp_vs_load_per_core_sorted = fig3.add_subplot(111)
+    plot_temp_vs_load_per_core_sorted(data, ax_temp_vs_load_per_core_sorted)
+
+    fig3.tight_layout()
 
 def plot_incomplete_data(data):
     fig = plt.figure()
