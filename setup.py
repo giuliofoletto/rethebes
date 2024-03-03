ad22df73cdd06724b4c3576635a77486e183dcad
Giulio Foletto <giulio.foletto@outlook.com>
2024-03-03 21:53:18 +0100

    Fix pip not installing subpackages correctly

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ Setup file to install rethebes as a package.
 Usage: pip install .
 
 Authors: Giulio Foletto.
+License: See project-level license file.
 """
 
 # Assign the content of README.md to long_description
 from os import path
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
@@ -23,8 +24,9 @@ setup(
     long_description_content_type="text/markdown",
     author="Giulio Foletto",
     author_email="giulio.foletto@outlook.com",
-    packages=["rethebes"],
-    package_dir={"rethebes": "rethebes"},
+    license="Apache",
+    license_files=("LICENSE.txt", "NOTICE.txt"),
+    packages=find_packages(),
     install_requires=["psutil", "pythonnet", "pyzmq", "pandas", "matplotlib", "numpy"],
     entry_points={"console_scripts": ["rethebes = rethebes.__main__:main"]},
     zip_safe=False,
