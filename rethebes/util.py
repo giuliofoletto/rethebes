279737cf2551405492c1863c8f6082fe15bdbb9c
Giulio Foletto <giulio.foletto@outlook.com>
2025-12-24 22:14:16 +0100

    Improve console output with rich

--- rethebes/util.py
+++ rethebes/util.py
@@ -6,16 +6,17 @@ License: See project-level license file.
 """
 
 import logging
-import sys
 from pathlib import Path
+
+from rich.logging import RichHandler
 
 
 def configure_logging():
     logging.basicConfig(
-        stream=sys.stdout,
         level=logging.INFO,
-        format="[%(asctime)s] %(levelname)s - %(message)s",
+        format="%(message)s",
         datefmt="%Y-%m-%dT%H:%M:%S",
+        handlers=[RichHandler(rich_tracebacks=True, markup=True)],
     )  # Note that time (and hence default logger) does not support %f
 
 
